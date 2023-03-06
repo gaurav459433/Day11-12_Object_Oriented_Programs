b0b10aea72f3a3134f978ce84db0de8bcc12034d
gaurav459433 <gaurav459433@gmail.com>
2023-03-06 17:10:47 +0530

    Welcome To Object Oriented Programs

---
+++ StockAccountManagement.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignmentsrfp259.day11and12.objectorientedprograms;
+
+public class StockAccountManagement {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Object Oriented Programs");
+    }
+}
